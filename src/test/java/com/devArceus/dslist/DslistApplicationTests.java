99458a195328953e3fb040c4b237029a97669524
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2024-11-12 10:43:39 -0300

    project created

---
+++ src/test/java/com/devArceus/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devArceus.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

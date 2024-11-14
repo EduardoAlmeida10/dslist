81cd0fe1a7369ab9e63afd15312028f9b3c11517
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2024-11-14 11:19:53 -0300

    Database seed update

--- src/main/java/com/devArceus/dslist/entities/GameList.java
+++ src/main/java/com/devArceus/dslist/entities/GameList.java
@@ -10,15 +10,15 @@ public class GameList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+    private String name;
 
     public GameList(){
 
     }
 
-    public GameList(Long id, String nome) {
+    public GameList(Long id, String name) {
         this.id = id;
-        this.nome = nome;
+        this.name = name;
     }
 
     public Long getId() {
@@ -30,11 +30,11 @@ public class GameList {
     }
 
     public String getNome() {
-        return nome;
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNome(String name) {
+        this.name = name;
     }
 
     @Override
